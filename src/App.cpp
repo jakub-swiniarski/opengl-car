7b2bf5f198387da861064e8c1730bd43dc34e1d9
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-06-25 15:06:25 +0200

    add new files

---
+++ src/App.cpp
@@ -0,0 +1,36 @@
+#include <GLFW/glfw3.h>
+
+#include "App.hpp"
+#include "Exception.hpp"
+
+void App::run(void) {
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+}
+
+App::App(void) {
+    window = glfwCreateWindow(
+        400,
+        300,
+        "Shift and Drift", 
+        nullptr, 
+        nullptr
+    );
+
+    if (!window) {
+        glfwTerminate();
+        throw Exception("Failed to initialize window.");
+    }
+
+    glfwMakeContextCurrent(window);
+    glEnable(GL_DEPTH_TEST);
+
+    run();
+
+    glfwTerminate();
+}
