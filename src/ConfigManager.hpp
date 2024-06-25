0792bd86ff82f5ebc8a63ccb3ebf36a7cc34eb0d
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-06-25 16:52:17 +0200

    config manager

---
+++ src/ConfigManager.hpp
@@ -0,0 +1,15 @@
+#ifndef CONFIGMANAGER_HPP
+#define CONFIGMANAGER_HPP
+
+#include <string>
+#include <unordered_map>
+
+class ConfigManager {
+    std::unordered_map<std::string, std::string> config;
+
+public:
+    ConfigManager(std::string filename);
+    std::string get_config(std::string str);
+};
+
+#endif // CONFIGMANAGER_HPP
