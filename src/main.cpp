d9464853754653e4b334e2b22e700678e2e55b87
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-06-25 15:06:16 +0200

    initialize a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
+#include <GLFW/glfw3.h>
+#include <exception>
 #include <iostream>
 
+#include "App.hpp"
+#include "Exception.hpp"
+
 int main(void) {
-    std::cout << "TEST\n";
+    try {
+        if(!glfwInit())
+            throw Exception("Failed to initialize glfw.");
+
+        App game;
+    } catch (std::exception &e) {
+        std::cerr << "Error: " << e.what() << '\n';
+        return -1;
+    }
 
     return 0;
 }
