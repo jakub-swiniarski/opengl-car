70af7fd200d11b561ba47941bd38c4f2d8b700b5
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-06-26 23:02:57 +0200

    enum for colors

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@
 #include "App.hpp"
 #include "Exception.hpp"
 
+namespace color { // TODO: these color codes don't work on windows
+    const std::string reset = "\033[0m";
+    const std::string red = "\033[31m";
+    const std::string yellow = "\033[33m";
+}
+
 int main(void) {
     try {
         if(!glfwInit())
@@ -13,12 +19,12 @@ int main(void) {
         App game;
     } catch (Exception &e) {
         if (e.is_fatal()) {
-            std::cerr << "\033[31m[Error]\033[0m " << e.what() << '\n'; // TODO: create an enum class for colors + these color codes don't work on windows
+            std::cerr << "[" << color::red << "Error" << color::reset << "] " << e.what() << '\n'; 
             return -1;
         } else
-            std::cerr << "\033[33m[Warning]\033[0m]" << e.what() << '\n'; // FIXME: warnings are not displayed, only fatal errors that crash the game are shown (try multithreading?)
+            std::cerr << "[" << color::yellow << "Warning" << color::reset << "]" << e.what() << '\n'; // FIXME: warnings are not displayed, only fatal errors that crash the game are shown (try multithreading?)
     } catch (std::exception &e) {
-        std::cerr << "\033[31m[Error]\033[0m " << e.what() << '\n';
+        std::cerr << "[" << color::red << "Error" << color::reset << "] " << e.what() << '\n';
         return -1;
     }
 
