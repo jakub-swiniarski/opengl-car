9e356f2a419748e1d41bb8f8810d4b543bf3e565
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-06-27 18:57:22 +0200

    setup perspective

--- src/App.cpp
+++ src/App.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+#include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
 #include "App.hpp"
@@ -12,6 +14,29 @@ void sd::App::run(void) {
 
         glfwPollEvents();
     }
+}
+
+void sd::App::setup_perspective(int width, int height, float fov) {
+    glEnable(GL_DEPTH_TEST);
+
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    float aspect_ratio = static_cast<float>(width) / static_cast<float>(height);
+    float near_plane = 0.1f;
+    float far_plane = 100.0f;
+    float f = 1.0f / tan(fov * 0.5f * M_PI / 180.0f);
+    float projection_matrix[] = {
+        f / aspect_ratio, 0.0f, 0.0f, 0.0f,
+        0.0f, f, 0.0f, 0.0f,
+        0.0f, 0.0f, (far_plane + near_plane) / (near_plane - far_plane), -1.0f,
+        0.0f, 0.0f, (2.0f * far_plane * near_plane) / (near_plane - far_plane), 0.0f
+    };
+
+    glMultMatrixf(projection_matrix);
+    glMatrixMode(GL_MODELVIEW);
+    
 }
 
 sd::App::App(void) {
@@ -41,6 +66,12 @@ sd::App::App(void) {
     }
 
     glfwMakeContextCurrent(window);
+
+    setup_perspective(
+        std::stoi(cfg_manager.get_config("window_width")),
+        std::stoi(cfg_manager.get_config("window_height")),
+        90.f
+    );
 
     run();
 
