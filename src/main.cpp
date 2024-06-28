f723d5cc4eae1e03eff22a80f12be5341c9cd880
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-06-28 17:33:07 +0200

    put log code in separate files

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,9 @@
 #include <GLFW/glfw3.h>
 #include <exception>
-#include <iostream>
 
 #include "App.hpp"
 #include "Exception.hpp"
-
-namespace sd {
-    namespace color { // TODO: these color codes don't work on windows
-        const std::string reset = "\033[0m";
-        const std::string red = "\033[31m";
-        const std::string yellow = "\033[33m";
-    }
-
-    void log(std::string label, std::string col, const char *msg);
-}
-
-void sd::log(std::string label, std::string col, const char *msg) {
-    std::cerr << "[" << col << label << sd::color::reset << "] " << msg << std::endl;
-}
+#include "log.hpp"
 
 int main(void) {
     try {
