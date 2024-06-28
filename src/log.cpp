d69db2e576c3c94ff90155abbd24ce9aead66a0b
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-06-28 17:48:51 +0200

    use std::string in log func

--- src/log.cpp
+++ src/log.cpp
@@ -6,7 +6,6 @@ const std::string sd::color::reset = "\033[0m";
 const std::string sd::color::red = "\033[31m";
 const std::string sd::color::yellow = "\033[33m";
 
-// TODO: try std::string msg and see if it works with .what()
-void sd::log(std::string label, std::string col, const char *msg) {
+void sd::log(std::string label, std::string col, std::string msg) {
     std::cerr << "[" << col << label << sd::color::reset << "] " << msg << std::endl;
 }
