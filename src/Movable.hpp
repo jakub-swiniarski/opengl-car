97926dd9e0bad67fb77e98598eaa0ed2d8d6dc54
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-07-08 19:57:27 +0200

    add new files

---
+++ src/Movable.hpp
@@ -0,0 +1,17 @@
+#ifndef MOVABLE_HPP
+#define MOVABLE_HPP
+
+#include "Renderable.hpp"
+
+namespace sd {
+    class Movable : public sd::Renderable {
+    protected:
+        GLfloat speed;
+
+    public:
+        Movable(std::string filename, sd::Vec3 pos, GLfloat yaw);
+        virtual void update(void);
+    };
+}
+
+#endif // MOVABLE_HPP
