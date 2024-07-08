ae7e340b5e3c8295474887b3ce6bcdca9348c8da
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-07-08 17:28:30 +0200

    renderable class

--- src/Car.hpp
+++ src/Car.hpp
@@ -4,19 +4,17 @@
 #include <GL/gl.h>
 
 #include "Keys.hpp"
-#include "Model.hpp"
+#include "Renderable.hpp"
 
 namespace sd {
-    class Car {
-        sd::Model model;
+    class Car : public sd::Renderable {
         GLfloat speed;
         const GLfloat accel;
         sd::Keys keys;
 
     public:
         Car(std::string filename, sd::Vec3 pos, GLfloat yaw, GLfloat accel);
-        void update(void); // TODO: move this to Model class or some Movable derivative of Model
-        const sd::Model &get_model(void) const;
+        void update(void);
         const sd::Keys &get_keys(void) const;
         void set_keys(sd::Keys &keys);
     };
