d1d6d0321473892037c95913d331ac5b789910a9
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2024-07-09 13:38:17 +0200

    input processor class

--- src/App.hpp
+++ src/App.hpp
@@ -3,19 +3,20 @@
 
 #include "Camera.hpp"
 #include "ConfigManager.hpp"
+#include "InputProcessor.hpp"
 #include "Model.hpp"
 
 class GLFWwindow;
 
 namespace sd {
     class App {
-        GLFWwindow*       window;
-        sd::ConfigManager cfg_manager;
-        sd::Camera        camera;
+        GLFWwindow*        window;
+        sd::ConfigManager  cfg_manager;
+        sd::Camera         camera;
+        sd::InputProcessor input_proc;
 
         void        run(void);
         void        render_model(const sd::Model& m) const;
-        static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
     public:
         App(void);
