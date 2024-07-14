13b89f8c283d57b0dbe1348eb4719a8a26b52b26
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-07-14 16:05:26 +0200

    remove temporary "lighting"

--- src/Model.hpp
+++ src/Model.hpp
@@ -12,7 +12,6 @@ namespace sd {
         sd::Vec3              pos;
         GLfloat               yaw;
         std::vector<sd::Vec3> verts;
-        std::vector<sd::Vec3> normals;
 
     public:
         Model(std::string filename, sd::Vec3 pos, GLfloat yaw);
@@ -20,7 +19,6 @@ namespace sd {
         const sd::Vec3&              get_pos(void) const;
         const GLfloat&               get_yaw(void) const;
         const std::vector<sd::Vec3>& get_verts(void) const;
-        const std::vector<sd::Vec3>& get_normals(void) const;
         void                         move(sd::Vec3 vec);
         void                         turn(GLfloat yaw);
     };
